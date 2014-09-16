d04f7f2b571400a1816f7ce505385520fd26fd1c
toddhollen <toddhollen@gmail.com>
2014-09-16 13:31:45 -0600

    Fixed syntax issues, added feature to clear between runs

--- js/app.js
+++ js/app.js
@@ -1,25 +1,29 @@
 $(document).ready(function () {
 
-    var num = $('#input-line').val();
-    var g = +num;
+        var fizzBuzz = function (g) {
 
-    var fizzBuzz = function (g) {
+            var num = $('#input-line').val();
+            var g = +num;
 
        if (g < 101 && g >= 1 && g % 1==0){
 
         for (var i = 1; i <= g; i++) {
             
             if (i % 15 == 0) {
-            $('body').append("<div>Fizz-Buzz</div>");} 
+            $('body').append("<div class='data'>Fizz-Buzz</div>");
+            } 
 
             else if (i % 5 == 0) {
-            $('body').append("<div>Buzz</div>");} 
+            $('body').append("<div class='data'>Buzz</div>");
+            } 
 
             else if (i % 3 == 0) {
-            $('body').append("<div>Fizz</div>");} 
+            $('body').append("<div class='data'>Fizz</div>");
+            } 
 
             else {
-            $('body').append("<div>" + i + "</div>");}
+            $('body').append("<div class='data'>" + i + "</div>");
+            }
             
         };
         
@@ -28,11 +32,13 @@ $(document).ready(function () {
      
     $(document).keydown(function (event) {
         if (event.which == 13) {
+            $('.data').remove();
             fizzBuzz();
         }
     });
 
     $(document).on('click', '#Enter', function(){
+         $('.data').remove();
         fizzBuzz();
     });
       
